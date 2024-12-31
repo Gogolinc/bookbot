89eda60e2046b4a397567cc4d12c06a5713988db
Gogolinc <94945992+Gogolinc@users.noreply.github.com>
2024-12-31 11:40:45 -0600

    Adds method for counting letters

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     number_of_words = count_words_in_book(text)
-    print(f"\nNumber of words:   {number_of_words}")
+    letter_counts = count_letters(text)
+    print(f"\nLetter counts:   {letter_counts}")
 
 def get_book_text(path):
-    with open("books/frankenstein.txt") as f:
-        print(f.read())
+    with open(path) as f:
+       return f.read()
 
-def count_words_in_book(text):
-    words = text.split()
+def count_words_in_book(t):
+    words = t.split()
     return len(words)
+
+def count_letters(text):
+    lowered_string = text.lower()
+    letter_counts = {}
+    for char in lowered_string:
+        letter_counts[char] = letter_counts.get(char, 0) + 1
+    return letter_counts
+
 
 main()
