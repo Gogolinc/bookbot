f037092a7166a116fd6690c98f2d00059b4525a8
Gogolinc <94945992+Gogolinc@users.noreply.github.com>
2024-12-31 11:13:51 -0600

    Adds function getting number of words

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    number_of_words = count_words_in_book(text)
+    print(f"\nNumber of words:   {number_of_words}")
+
+def get_book_text(path):
     with open("books/frankenstein.txt") as f:
-        contents = f.read()
-        print(contents)
+        print(f.read())
+
+def count_words_in_book(text):
+    words = text.split()
+    return len(words)
 
 main()
