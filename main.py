caa5962e35445d8b738daae21ce0c2f4eb3e24fb
Gogolinc <94945992+Gogolinc@users.noreply.github.com>
2024-12-31 12:05:46 -0600

    Regex for filtering out non alpha characters

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import re
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     number_of_words = count_words_in_book(text)
     letter_counts = count_letters(text)
-    print(f"\nLetter counts:   {letter_counts}")
+    format_report(book_path, number_of_words, letter_counts)
 
 def get_book_text(path):
     with open(path) as f:
@@ -15,10 +16,17 @@ def count_words_in_book(t):
 
 def count_letters(text):
     lowered_string = text.lower()
+    lowered_string = re.sub(r'[^A-Za-z\\s]', '', lowered_string)
     letter_counts = {}
     for char in lowered_string:
         letter_counts[char] = letter_counts.get(char, 0) + 1
     return letter_counts
+
+def format_report(path, number_of_words, letter_counts):
+    print(f"--- Begin report of {path} ---\n")
+    print(f"{number_of_words} words forund in the document \n\n")
+    for lc in letter_counts:
+        print(f"The '{lc}' character was found {letter_counts[lc]} times")
 
 
 main()
